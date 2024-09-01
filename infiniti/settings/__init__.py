2254cf3e97f3030eb4dfd0976e17f5ee3ddb1184
sajad j <sjorjs.job@gmail.com>
2024-09-01 22:20:52 +0330

    Initiate infiniti project base

---
+++ infiniti/settings/__init__.py
@@ -0,0 +1,5 @@
+from infiniti.settings.base import *
+from infiniti.settings.apps import *
+from infiniti.settings.database import *
+from infiniti.settings.middleware import *
+from infiniti.settings.static import *
---
+++ infiniti/settings/database.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
